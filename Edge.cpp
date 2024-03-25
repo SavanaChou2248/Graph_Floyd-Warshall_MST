11128691bd4574f67ff389a2b8177d71773394fb
Savana Chou <123604436+SavanaChou2248@users.noreply.github.com>
2024-03-25 16:42:55 -0700

    Update Edge.cpp

--- Edge.cpp
+++ Edge.cpp
@@ -1,6 +1,5 @@
 /*
 
-
 Brief Overview:
 This class, Edge, represents an edge in a graph. An edge connects two vertices and has a weight associated with it. This class is part of the implementation for a graph-related programming assignment.
 
