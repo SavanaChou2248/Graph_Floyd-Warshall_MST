6ffccc2443514402dec692586a4411b0657d1c61
Savana Chou <123604436+SavanaChou2248@users.noreply.github.com>
2024-03-25 16:43:20 -0700

    Update EdgeComparer.cpp

--- EdgeComparer.cpp
+++ EdgeComparer.cpp
@@ -1,9 +1,4 @@
 /*
-Savana Chou
-2428859
-sachou@chapman.edu
-CPSC350-02
-Programming Assignment 6: Spanning the Gamut
 
 Brief Overview:
 Contains the implementation of a custom comparison function for edges. The comparison is based on the weights of the edges. 
